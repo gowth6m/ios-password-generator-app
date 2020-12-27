539328a446685a210e2c9374a5e842e2c1e2b150
Gowtham <gowtham4n@gmail.com>
2020-12-27 00:08:16 +0000

    Init

---
+++ password generator/password generator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  password generator
+//
+//  Created by Gowtham Ravindrathas on 27/12/2020.
+//  Copyright © 2020 Gowthaman Ravindrathas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
